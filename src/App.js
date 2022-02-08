7063edda8735c1ccbf75207fe362efb10da9bdbf
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-07 22:58:11 -0500

    adding mock data and consts

--- src/App.js
+++ src/App.js
@@ -21,16 +21,17 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { Add, ArrowForward, Dashboard, Mail } from "@mui/icons-material";
-
-const LEFT = 'left';
-
-const TITLE = <>Senior Project App</>;
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import { LEFT, OFFERING, POST_TYPE, TITLE } from "./Consts";
+import { posts } from "./MockData";
+import { Button, Chip, Stack } from "@mui/material";
 
 function App() {
   const [state, setState] = React.useState({
     [LEFT]: true,
   });
-
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -126,6 +127,13 @@ function App() {
     </IconButton>
   )
 
+  const Item = styled(Paper)(({theme}) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
   return (
     <div className="App">
       <AppBar position="static">
@@ -134,9 +142,9 @@ function App() {
             <MenuIcon/>
           </IconButton>
           <Typography variant="h6" color="inherit" component="div">
-            {<Typography variant="h6" color="inherit" component="div">
+            <Typography variant="h6" color="inherit" component="div">
               {TITLE}
-            </Typography>}
+            </Typography>
           </Typography>
           <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
             <Tab label="All" {...a11yProps(0)} />
@@ -171,7 +179,120 @@ function App() {
         <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
         </Box>
         <TabPanel value={value} index={0}>
-          All
+          <Box sx={{flexGrow: 1}}>
+            <Grid container spacing={2}>
+              {posts.map(p => {
+                  if (p[POST_TYPE] === OFFERING) {
+                    return <Grid item xs={6}>
+                      <Item>
+                        <Item>
+
+                          <h1>
+                            <Stack direction="row" spacing={1}>
+                              {p.title}
+                              <Chip label={p.userType}/>
+                              <Chip label={p.date} variant="outlined"/>
+                            </Stack>
+                          </h1>
+                          {p.details}
+
+                          <Grid container item spacing={2}>
+                            <Grid item xs={6}>
+                              <h2>
+                                Skills
+                              </h2>
+                              {p.skills}
+                            </Grid>
+                            <Grid item xs={6}>
+                              <h2>
+                                Software
+                              </h2>
+                              {p.software}
+                            </Grid>
+                          </Grid>
+
+                          <Grid container item spacing={2}>
+                            <Grid item xs={4}>
+                              <h2>
+                                Advisor
+                              </h2>
+                            </Grid>
+                            <Grid item xs={8}>
+                              <h2>
+                                {p.advisor}
+                              </h2>
+                            </Grid>
+                          </Grid>
+                          <h2>Members</h2>
+
+                          {p.members.map(m =>
+                            <div>{m}</div>
+                          )}
+
+                        </Item>
+
+                        <Item>
+                          <Stack direction="row" spacing={1}>
+                            <Chip label="6/6 Members"/>
+                            <Button variant={"contained"}>Request to Join</Button>
+                            <Button variant={"contained"}>Edit</Button>
+                          </Stack>
+                        </Item>
+
+                      </Item>
+                    </Grid>
+
+                  } else {
+                    return <Grid item xs={6}>
+                      <Item>
+                        <Item>
+
+                          <h1>
+                            <Stack direction="row" spacing={1}>
+                              {p.title}
+                            </Stack>
+                          </h1>
+                          {p.details}
+                          <Grid container item spacing={2}>
+                            <Grid item xs={4}>
+                              <h2>
+                                Name
+                              </h2>
+                            </Grid>
+                            <Grid item xs={8}>
+                              <h2>
+                                {p.name}
+                              </h2>
+                            </Grid>
+                          </Grid>
+                          <Grid container item spacing={2}>
+                            <Grid item xs={4}>
+                              <h2>
+                                Contact
+                              </h2>
+                            </Grid>
+                            <Grid item xs={8}>
+                              <h2>
+                                {p.contact}
+                              </h2>
+                            </Grid>
+                          </Grid>
+                        </Item>
+                        <Item>
+                          <Stack direction="row" spacing={1}>
+                            <Button variant={"contained"}>Add to My Group</Button>
+                            <Button variant={"contained"}>Message</Button>
+                          </Stack>
+                        </Item>
+                      </Item>
+                    </Grid>
+                  }
+
+                }
+              )}
+            </Grid>
+          </Box>
+
         </TabPanel>
         <TabPanel value={value} index={1}>
           Student
---
+++ src/Consts.js
@@ -0,0 +1,5 @@
+export const LEFT = 'left';
+export const TITLE = 'Senior Project App';
+export const SEEKING = 'seeking';
+export const OFFERING = 'offering';
+export const POST_TYPE = "postType"
