0608f8c2e16a53ff60fed59e41682aa0a93ab4ad
giselle <gutierrez.gi@ufl.edu>
2022-02-18 00:47:35 -0500

    Added UF logo to Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,66 +1,75 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Button } from "@mui/material";
+import {Button, ImageList, ImageListItem, ImageListItemBar} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import GlobalState from "../state/GlobalState";
 
 
 const Login = () => {
-  const {
-    loginUser,
-  } = React.useContext(GlobalState);
+    const {
+        loginUser,
+    } = React.useContext(GlobalState);
 
-  return <React.Fragment>
+    return <React.Fragment>
+
+        <img
+            src={'https://www.uff.ufl.edu/wp/wp-content/uploads/2020/02/logo-uf-primary.png'}
+            alt={"UF Logo"}
+            style={{height: 138.908/3,
+                width: 757/3,
+                margin: 44.8462/5
+            }}
+        />
 
 
-    <Box
-      component="form"
-      sx={{
-        display: "flex", flex: "1", justifyContent: "center"
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Typography variant={"h1"}>Senior Project Database</Typography>
-    </Box>
-    <Box
-      component="form"
-      sx={{
-        display: "flex", flex: "1", justifyContent: "center"
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Typography variant="h6" component="h6">
-        Email
-      </Typography>
-    </Box>
+        <Box
+            component="form"
+            sx={{
+                display: "flex", flex: "1", justifyContent: "center", mt: 2
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <Typography sx={{color: "darkgray"}} variant={"h2"} mt={3}>Senior Project Database</Typography>
+        </Box>
+        <Box
+            component="form"
+            sx={{
+                display: "flex", flex: "1", justifyContent: "center", mt: 8
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <Typography variant="h6" component="h6">
+                Email
+            </Typography>
+        </Box>
 
-    <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
-      <TextField required
-                 id="outlined-basic"
-                 variant="outlined"/>
-    </Box>
-    <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
-      <Typography variant="h6" component="h6">
-        Password
-      </Typography>
-    </Box>
-    <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
-      <TextField
-        required
-        id="password-input"
-        type="password"
-        autoComplete="current-password"
+        <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
+            <TextField required
+                       id="outlined-basic"
+                       variant="outlined"/>
+        </Box>
+        <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
+            <Typography variant="h6" component="h6">
+                Password
+            </Typography>
+        </Box>
+        <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
+            <TextField
+                required
+                id="password-input"
+                type="password"
+                autoComplete="current-password"
 
-      />
-    </Box>
-    <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
-      <Button variant="contained" component="span" sx={{mt: 3}} onClick={loginUser}>
-        Login
-      </Button>
-    </Box>
-  </React.Fragment>
+            />
+        </Box>
+        <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
+            <Button variant="contained" component="span" sx={{mt: 3}} onClick={loginUser}>
+                Login
+            </Button>
+        </Box>
+    </React.Fragment>
 }
 export default Login;
