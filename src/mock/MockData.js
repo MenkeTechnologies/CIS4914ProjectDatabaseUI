1e1a95afb99d4f30fd475c191d822e3dac6a4cb3
Andrew Garmon <andrew.garmon@gmail.com>
2022-02-20 17:23:31 -0500

    Finished tab nav

--- src/mock/MockData.js
+++ src/mock/MockData.js
@@ -5,6 +5,7 @@ const projectPostDummy = {
   [POST_TYPE]: OFFERING,
   userType: FACULTY,
   date: new Date().toUTCString(),
+  name: "John Doe",
   details: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque delectus deserunt dignissimos doloremque doloribus dolorum ducimus earum error et explicabo facere fugit illum ipsam ipsum laboriosam laudantium molestiae, nam natus non nostrum numquam odit omnis provident quae quasi qui quisquam quos recusandae reiciendis reprehenderit repudiandae soluta tempora vero. Corporis enim ipsum modi numquam officiis. A accusamus ad aspernatur assumenda atque aut autem beatae consectetur consequatur cumque cupiditate dolorem dolorum ducimus earum est exercitationem id illum iste, itaque laboriosam laudantium maiores nobis numquam odit officia qui quidem repellat repudiandae sed sequi tempore, tenetur ullam ut vel veritatis vero voluptas. Quod, unde.",
   skills: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut culpa delectus deserunt dolore explicabo fuga fugit laborum laudantium, maxime minus, mollitia neque pariatur quia quos reprehenderit, sit tenetur veniam.',
   software: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut culpa delectus deserunt dolore explicabo fuga fugit laborum laudantium, maxime minus, mollitia neque pariatur quia quos reprehenderit, sit tenetur veniam.',
@@ -27,7 +28,12 @@ const lookingPostDummy = {
   contact: 'helloworld@domain.tld',
   details: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque delectus deserunt dignissimos doloremque doloribus dolorum ducimus earum error et explicabo facere fugit illum ipsam ipsum laboriosam laudantium molestiae, nam natus non nostrum numquam odit omnis provident quae quasi qui quisquam quos recusandae reiciendis reprehenderit repudiandae soluta tempora vero. Corporis enim ipsum modi numquam officiis. A accusamus ad aspernatur assumenda atque aut autem beatae consectetur consequatur cumque cupiditate dolorem dolorum ducimus earum est exercitationem id illum iste, itaque laboriosam laudantium maiores nobis numquam odit officia qui quidem repellat repudiandae sed sequi tempore, tenetur ullam ut vel veritatis vero voluptas. Quod, unde."
 }
-
+const messageDummy = {
+  name: "Jane Doe", 
+  messageTime: "1/1/2022 12:45 PM", 
+  messageTitle: "Inquiry about your project", 
+  messageBody: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque delectus deserunt dignissimos doloremque doloribus dolorum ducimus earum error et explicabo facere fugit illum ipsam ipsum laboriosam laudantium molestiae, nam natus non nostrum numquam odit omnis provident quae quasi qui quisquam quos recusandae reiciendis reprehenderit repudiandae soluta tempora vero. Corporis enim ipsum modi numquam officiis. A accusamus ad aspernatur assumenda atque aut autem beatae consectetur consequatur cumque cupiditate dolorem dolorum ducimus earum est exercitationem id illum iste, itaque laboriosam laudantium maiores nobis numquam odit officia qui quidem repellat repudiandae sed sequi tempore, tenetur ullam ut vel veritatis vero voluptas. Quod, unde."
+}
 export const posts = [
   projectPostDummy,
   projectPostDummy,
@@ -82,4 +88,17 @@ export const posts = [
   lookingPostDummy,
   projectPostDummy,
   projectPostDummy,
+]
+
+export const messages = [
+  messageDummy,
+  messageDummy,
+  messageDummy,
+  messageDummy,
+  messageDummy,
+  messageDummy,
+  messageDummy,
+  messageDummy,
+  messageDummy,
+  messageDummy
 ]
