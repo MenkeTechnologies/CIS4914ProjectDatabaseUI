5a1dd504463a0f7ecd2f6ed1d01d00eed898af2a
MenkeTechnologies <linux.dev25@gmail.com>
2022-02-20 21:12:34 -0500

    add services

---
+++ src/service/Auth.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const checkUser = (email, password) => {
+
+  axios.post('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
