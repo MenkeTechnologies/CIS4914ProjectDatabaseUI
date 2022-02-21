d162694284879630139c238058b9575dd54496ec
giselle <gutierrez.gi@ufl.edu>
2022-02-20 19:10:15 -0500

    email and password validation in Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,86 +1,129 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Button } from "@mui/material";
+import {Avatar, Button, Checkbox, FormControlLabel, ImageList, ImageListItem, ImageListItemBar} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import GlobalState from "../state/GlobalState";
-import { TITLE } from "../util/Consts";
+import {TITLE} from "../util/Consts";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import LoginIcon from '@mui/icons-material/Login';
+import {Form, Field, Formik, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
 
 
 const Login = () => {
-  const {
-    loginUser,
-  } = React.useContext(GlobalState);
+    const {
+        loginUser,
+    } = React.useContext(GlobalState);
 
-  return <React.Fragment>
+    const paperStyle = {padding: 20, height: '55vh', width: 300, margin: '30px auto'}
+    const avatarStyle = {backgroundColor: 'rgba(255,103,0,0.83)'}
+    const btnstyle = {margin: '20px 0'}
+    const initialValues = {
+        username: '',
+        password: '',
+        remember: false
+    }
+    const validationSchema = Yup.object().shape({
+        username: Yup.string().email('Please enter valid email').required("Required"),
+        password: Yup.string().required("Required")
+    })
+    const onSubmit = (values) => {
+        console.log(values)
+        // setTimeout(() => {
+        //     props.resetForm()
+        //     props.setSubmitting(false)
+        // }, 2000)
+    }
+    const onKeyDown = (e)=>{
 
-    <Box
-      component="form"
-      sx={{
-        display: "flex", flex: "1", justifyContent: "center", mt: 2
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <img
-        src={require('../img/logo-uf-primary.png')}
-        alt={"UF Logo"}
-        style={{
-          height: 138.908,
-          width: 757,
-          margin: 44.8462 / 5
-        }}
-      />
-    </Box>
+    }
+
+    return <React.Fragment>
+        <Box
+            component="form"
+            sx={{
+                display: "flex", flex: "1", justifyContent: "center", mt: 2
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <img
+                src={require('../img/logo-uf-primary.png')}
+                alt={"UF Logo"}
+                style={{
+                    height: 138.908 / 2.5,
+                    width: 757 / 2.5,
+                    margin: 44.8462 / 5
+                }}
+            />
+        </Box>
+        <Box
+            component="form"
+            sx={{
+                display: "flex", flex: "1", justifyContent: "center", mt: 0
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <Typography sx={{color: "#C0C0C0"}} variant={"h2"} mt={7}>{TITLE}</Typography>
+        </Box>
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}><LoginIcon/></Avatar>
+                </Grid>
+                <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                    {(props) => (
+                        <Form>
+                            <Field as={TextField}
+                                   label='Username'
+                                   onKeyDown={onKeyDown}
+                                   name='username'
+                                   placeholder='Enter username'
+                                   fullWidth required
+                                   sx={{mt: 3}}
+                                   helperText={<ErrorMessage name="username"/>}
+                            />
+                            <Field as={TextField}
+                                   label='Password'
+                                   onKeyDown={onKeyDown}
+                                   name='password'
+                                   placeholder='Enter Password'
+                                   type='password'
+                                   fullWidth required
+                                   autoComplete="current-password"
+                                   sx={{mt: 3, mb: 3}}
+                                   helperText={<ErrorMessage name="password"/>}
+                            />
+                            <Field as={FormControlLabel}
+                                   name='remember'
+                                   control={
+                                       <Checkbox
+                                           color='primary'
+                                       />
+                                   }
+                                   label="Remember me"
+                            />
+                            <Button type='Submit' variant="contained"
+                                    disabled={props}
+                                    onClick={loginUser}
+                                    component="span"
+                                    color='primary'
+                                    sx={{backgroundColor: "rgba(0,78,152,0.88)"}}
+                                    style={btnstyle}
+                                    fullWidth>{props.isSubmitting ? "Loading" : "Sign in"}
+                            </Button>
+
+                        </Form>
+                    )}
+                </Formik>
+
+            </Paper>
+        </Grid>
 
 
-    <Box
-      component="form"
-      sx={{
-        display: "flex", flex: "1", justifyContent: "center", mt: 2
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Typography sx={{color: "darkgray"}} variant={"h2"} mt={3}>{TITLE}</Typography>
-    </Box>
-    <Box
-      component="form"
-      sx={{
-        display: "flex", flex: "1", justifyContent: "center", mt: 8
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Typography variant="h6" component="h6">
-        Email
-      </Typography>
-    </Box>
-
-    <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
-      <TextField required
-                 id="outlined-basic"
-                 variant="outlined"/>
-    </Box>
-    <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
-      <Typography variant="h6" component="h6">
-        Password
-      </Typography>
-    </Box>
-    <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
-      <TextField
-        required
-        id="password-input"
-        type="password"
-        autoComplete="current-password"
-
-      />
-    </Box>
-    <Box sx={{display: "flex", flex: "1", justifyContent: "center"}}>
-      <Button variant="contained" component="span" sx={{mt: 3}} onClick={loginUser}>
-        Login
-      </Button>
-    </Box>
-  </React.Fragment>
+    </React.Fragment>
 }
 export default Login;
