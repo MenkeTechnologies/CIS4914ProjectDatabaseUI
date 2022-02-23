52b771b66437bf471ef423e5d747c2bf0698cbb1
giselle <gutierrez.gi@ufl.edu>
2022-02-22 23:24:04 -0500

    Formatted SeekingPostForm.js

--- src/components/SeekingPostForm.js
+++ src/components/SeekingPostForm.js
@@ -1,125 +1,125 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Avatar, Button, Checkbox, FormControlLabel } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import {Avatar, Button, Checkbox, FormControlLabel, Stack} from "@mui/material";
 import GlobalState from "../state/GlobalState";
-import { BLUE, DK_GRAY, emptyOrInvalid, FACULTY, LT_GRAY, ORANGE, STUDENT, TITLE } from "../util/Consts";
+import {BLUE, DK_GRAY, emptyOrInvalid, FACULTY, LT_GRAY, ORANGE, STUDENT, TITLE} from "../util/Consts";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import LoginIcon from '@mui/icons-material/Login';
-import { ErrorMessage, Field, Form, Formik } from 'formik'
+import {ErrorMessage, Field, Form, Formik} from 'formik'
 import * as Yup from 'yup'
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
 
 
-const SeekingPostForm = () => {
-  const {
-    notRegistering,
-    registerUser
-  } = React.useContext(GlobalState);
+const OfferingPostForm = () => {
+    const {
+        notRegistering,
+        registerUser
+    } = React.useContext(GlobalState);
 
-  const paperStyle = {padding: 20, width: 300, margin: '30px auto'}
-  const avatarStyle = {backgroundColor: ORANGE}
-  const btnStyle = {margin: '20px 0'}
-  const initialValues = {
-    username: '',
-    email: '',
-    password: '',
-    userType: ''
-  }
-  const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Please enter valid email').required("Required"),
-    password: Yup.string()
-      .min(8, 'Password is too short - should be 8 chars minimum.')
-      .required("Required")
-  })
+    const paperStyle = {
+        padding: 20,
+        margin: '30px auto',
+        display: 'grid',
+        height: '100%',
+    }
+    const btnStyle = {margin: '20px 5px'}
+    const initialValues = {
+        topic: '',
+        preferredContact: '',
+        password: '',
+        userType: ''
+    }
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email('Please enter valid email').required("Required"),
+        password: Yup.string()
+            .min(8, 'Password is too short - should be 8 chars minimum.')
+            .required("Required")
+    })
 
-  return <React.Fragment>
-    <Grid>
-      <Paper elevation={10} style={paperStyle}>
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={(v) => {
-          registerUser(v.username)
-        }}>
-          {(props) => (
-            <Form>
-              <Field as={TextField}
-                     label='Username'
-                     name='username'
-                     placeholder='Enter username'
-                     fullWidth required
-                     sx={{mt: 3}}
-                     helperText={<ErrorMessage name="username"/>}
-              />
-              <FormControl fullWidth sx={{mt: 2}}>
-                <InputLabel id="demo-simple-select-helper-label">User Type</InputLabel>
-                <Field as={Select}
-                       labelId="demo-simple-select-helper-label"
-                       id="demo-simple-select-helper"
-                       label="User Type"
-                       name='userType'
-                       fullWidth required
-                       helperText={<ErrorMessage name="userType"/>}
-                >
-                  <MenuItem value={STUDENT}>Student</MenuItem>
-                  <MenuItem value={FACULTY}>Faculty</MenuItem>
-                </Field>
-              </FormControl>
-              <Field as={TextField}
-                     label='Email'
-                     name='email'
-                     placeholder='Enter email'
-                     fullWidth required
-                     sx={{mt: 3}}
-                     helperText={<ErrorMessage name="email"/>}
-              />
-              <Field as={TextField}
-                     label='Password'
-                     name='password'
-                     placeholder='Enter Password'
-                     type='password'
-                     fullWidth required
-                     autoComplete="current-password"
-                     sx={{mt: 3, mb: 3}}
-                     helperText={<ErrorMessage name="password"/>}
-              />
-              <Field as={FormControlLabel}
-                     name='remember'
-                     control={
-                       <Checkbox
-                         color='primary'
-                       />
-                     }
-                     label="Remember me"
-              />
-              <Button type='Submit' variant="contained"
-                      disabled={emptyOrInvalid(props)}
-                      color='primary'
-                      sx={{backgroundColor: BLUE}}
-                      style={btnStyle}
-                      fullWidth>{"SeekingPostForm"}
-              </Button>
+    return <React.Fragment>
+        <Grid container spacing={'spacing'}>
+            <Paper elevation={10} style={paperStyle}>
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={(v) => {
+                    registerUser(v.username)
+                }}>
+                    {(props) => (
+                        <Form>
+                            <Grid container item spacing={2}>
+                                <Grid item xs={6}>
+                                    <Field as={TextField}
+                                           label='Name'
+                                           name='Name'
+                                           placeholder='Name'
+                                           fullWidth required
+                                           sx={{mt: 3}}
+                                           helperText={<ErrorMessage name="topic"/>}
+                                    />
 
-              <Button type={"button"} variant="contained"
-                      onClick={notRegistering}
-                      color='primary'
-                      sx={{backgroundColor: BLUE}}
-                      style={btnStyle}
-                      fullWidth>{"Login"}
-              </Button>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Field as={TextField}
+                                           label='Preferred Contact'
+                                           name='preferredContact'
+                                           placeholder='Enter preferred contact'
+                                           fullWidth required
+                                           sx={{mt: 3}}
+                                           helperText={<ErrorMessage name="preferredContact"/>}
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Field as={TextField}
+                                   label='Summary'
+                                   name='Summary'
+                                   placeholder='Project Summary'
+                                   multiline
+                                   rows={4}
+                                   fullWidth required
+                                   sx={{mt: 3}}
+                                   helperText={<ErrorMessage name="Project Summary"/>}
 
+                            />
+                            <Field as={TextField}
+                                   label='Other members'
+                                   name='Members'
+                                   placeholder='List of other students'
+                                   sx={{
+                                       mt: 3,
+                                       ml: 9,
+                                       width: '70%',
+                                       display: 'flex',
 
-            </Form>
-          )}
-        </Formik>
+                                   }}
+                            />
 
-      </Paper>
-    </Grid>
+                            <Stack direction='row' spacing={2} justifyContent='center'>
+                                <Button type={"button"} variant="contained"
+                                        onClick={notRegistering}
+                                        color='primary'
+                                        sx={{backgroundColor: BLUE}}
+                                        style={btnStyle}
+                                        startIcon={<DeleteIcon/>}
+                                >Clear
+                                </Button>
+                                <Button type='Post' variant="contained"
+                                        disabled={emptyOrInvalid(props)}
+                                        color='primary'
+                                        sx={{backgroundColor: BLUE}}
+                                        style={btnStyle}
+                                        endIcon={<SendIcon/>}
+                                >
+                                    Post
+                                </Button>
+                            </Stack>
 
+                        </Form>
+                    )}
+                </Formik>
 
-  </React.Fragment>
+            </Paper>
+        </Grid>
+
+    </React.Fragment>
 }
-export default SeekingPostForm;
+export default OfferingPostForm;
+
