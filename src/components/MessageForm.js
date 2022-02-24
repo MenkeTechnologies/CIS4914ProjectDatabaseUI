ef2800eb7e649795f256fde6823fa724527503b7
Andrew Garmon <andrew.garmon@gmail.com>
2022-02-23 20:45:07 -0500

    touchup on MessageForm

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,122 +1,114 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
-import { Button, Checkbox, FormControlLabel } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import GlobalState from "../state/GlobalState";
-import { BLUE, FACULTY, ORANGE, STUDENT } from "../util/Consts";
+import { BLUE } from "../util/Consts";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 import * as Yup from 'yup'
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
 
 
 const MessageForm = () => {
   const {
-    notRegistering,
-    registerUser
+    notRegistering, registerUser
   } = React.useContext(GlobalState);
 
-  const paperStyle = {padding: 20, width: 300, margin: '30px auto'}
-  const avatarStyle = {backgroundColor: ORANGE}
-  const btnStyle = {margin: '20px 0'}
+  const paperStyle = {
+    padding: 20, margin: '30px auto', display: 'grid', height: '100%', width: "25%"
+  }
+  const btnStyle = {margin: '20px 5px'}
   const initialValues = {
-    username: '',
-    email: '',
-    password: '',
-    userType: ''
+    toName: '',
+    subject: '',
+    message: ''
   }
   const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Please enter valid email').required("Required"),
-    password: Yup.string()
-      .min(8, 'Password is too short - should be 8 chars minimum.')
-      .required("Required")
+    username: Yup.string().required("Required"),
+    subject: Yup.string().required("Required"),
+    message: Yup.string().required("Required"),
   })
 
+  const clear = props => {
+    Object.keys(props.values).forEach(v => props.values[v] = '');
+  };
+
   return <React.Fragment>
-    <Grid>
+
+    <Grid container spacing={'spacing'}>
       <Paper elevation={10} style={paperStyle}>
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={(v) => {
-          registerUser(v.username)
-        }}>
-          {(props) => (
-            <Form>
-              <Field as={TextField}
-                     label='Username'
-                     name='username'
-                     placeholder='Enter username'
-                     fullWidth required
-                     sx={{mt: 3}}
-                     helperText={<ErrorMessage name="username"/>}
-              />
-              <FormControl fullWidth sx={{mt: 2}}>
-                <InputLabel id="demo-simple-select-helper-label">User Type</InputLabel>
-                <Field as={Select}
-                       labelId="demo-simple-select-helper-label"
-                       id="demo-simple-select-helper"
-                       label="User Type"
-                       name='userType'
+        <Typography variant='h4' align='center'>
+          New Message
+        </Typography>
+        <Formik initialValues={initialValues} isInitialValid={false} validateOnMount={true}
+                validationSchema={validationSchema}
+                onSubmit={(v) => {
+                  registerUser(v.username)
+                }}>
+          {(props) => (<Form>
+            <Grid container item spacing={2}>
+              <Grid item xs={12}>
+                <Field as={TextField}
+                       label='Username'
+                       name='username'
+                       placeholder='Username'
                        fullWidth required
-                       helperText={<ErrorMessage name="userType"/>}
-                >
-                  <MenuItem value={STUDENT}>Student</MenuItem>
-                  <MenuItem value={FACULTY}>Faculty</MenuItem>
-                </Field>
-              </FormControl>
-              <Field as={TextField}
-                     label='Email'
-                     name='email'
-                     placeholder='Enter email'
-                     fullWidth required
-                     sx={{mt: 3}}
-                     helperText={<ErrorMessage name="email"/>}
-              />
-              <Field as={TextField}
-                     label='Password'
-                     name='password'
-                     placeholder='Enter Password'
-                     type='password'
-                     fullWidth required
-                     autoComplete="current-password"
-                     sx={{mt: 3, mb: 3}}
-                     helperText={<ErrorMessage name="password"/>}
-              />
-              <Field as={FormControlLabel}
-                     name='remember'
-                     control={
-                       <Checkbox
-                         color='primary'
-                       />
-                     }
-                     label="Remember me"
-              />
-              <Button type='Submit' variant="contained"
+                       sx={{mt: 3}}
+                       helperText={<ErrorMessage name="To"/>}
+                />
+
+              </Grid>
+              <Grid item xs={12}>
+                <Field as={TextField}
+                       label='Subject'
+                       name='subject'
+                       placeholder='Enter Subject'
+                       fullWidth required
+                       sx={{mt: 3}}
+                       helperText={<ErrorMessage name="Subject"/>}
+                />
+              </Grid>
+            </Grid>
+            <Field as={TextField}
+                   label='Message'
+                   name='message'
+                   placeholder='Message'
+                   multiline
+                   rows={4}
+                   fullWidth required
+                   sx={{mt: 3}}
+                   helperText={<ErrorMessage name="Message"/>}
+
+            />
+
+            <Stack direction='row' spacing={2} justifyContent='center'>
+              <Button type={"reset"} variant="contained"
+                      color='primary'
+                      sx={{backgroundColor: BLUE}}
+                      style={btnStyle}
+                      startIcon={<DeleteIcon/>}
+              >Clear
+              </Button>
+              <Button type={"submit"} variant="contained"
                       disabled={!props.isValid}
                       color='primary'
                       sx={{backgroundColor: BLUE}}
                       style={btnStyle}
-                      fullWidth>{"MessageForm"}
+                      endIcon={<SendIcon/>}
+              >
+                Send
               </Button>
+            </Stack>
 
-              <Button type={"button"} variant="contained"
-                      onClick={notRegistering}
-                      color='primary'
-                      sx={{backgroundColor: BLUE}}
-                      style={btnStyle}
-                      fullWidth>{"Login"}
-              </Button>
-
-
-            </Form>
-          )}
+          </Form>)}
         </Formik>
 
       </Paper>
     </Grid>
 
-
   </React.Fragment>
 }
 export default MessageForm;
+
