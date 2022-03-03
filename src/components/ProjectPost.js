52b328eb420dd969830e68ba20ad67a8f0288572
Carlson Joseph <carl2603@gmail.com>
2022-03-02 21:41:46 -0500

    feature/first-post-view

--- src/components/ProjectPost.js
+++ src/components/ProjectPost.js
@@ -1,72 +1,76 @@
 import React from 'react';
 import Grid from "@mui/material/Grid";
-import { Button, Chip, Stack } from "@mui/material";
+import { Button, Chip, Stack, Paper, Typography, List, ListItem, ListItemText } from "@mui/material";
 import { ACTIVE_TAB, Item, MESSAGES_TAB, PROJECT_TAB, SORT_BY, STATE } from "../util/Consts";
 import GlobalState from "../state/GlobalState";
-import AccountCircle from "@mui/icons-material/AccountCircle"
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import moment from 'moment'
+import { grid } from '@mui/system';
 
-const ProjectPost = ({post: {title, name, userType, date, details, skills, software, memberList, advisor}}) => {
+const ProjectPost = ({ post: { topic, name, userType, date, summary, skillsList, softwareList, advisor, memberList } }) => {
 
   const {
-    [STATE]: {[ACTIVE_TAB]: activeTab, [SORT_BY]: sortBy}, hideDrawer, handleNavChange, hideDrawerAndSetTab,
+    [STATE]: { [ACTIVE_TAB]: activeTab, [SORT_BY]: sortBy }, hideDrawer, handleNavChange, hideDrawerAndSetTab,
   } = React.useContext(GlobalState);
 
   return <Grid item xs={6}>
-    <Item>
-      <Item>
-
-        <h1>
-          <Stack direction="row" spacing={1}>
-            {title}
-            <Chip label={userType}/>
-            <Chip label={date} variant="outlined"/>
-            <Chip icon={<AccountCircle/>} label={name}/>
-          </Stack>
-        </h1>
-        {details}
-
-        <Grid container item spacing={2}>
-          <Grid item xs={6}>
-            <h2>
-              Skills
-            </h2>
-            {skills}
-          </Grid>
-          <Grid item xs={6}>
-            <h2>
-              Software
-            </h2>
-            {software}
-          </Grid>
+    <Paper elevation={3}>
+      <Grid container justifyContent="center">
+        <Grid item xs={8}>
+          <Typography variant="h4">{topic}</Typography>
         </Grid>
-
-        <Grid container item spacing={2}>
-          <Grid item xs={4}>
-            <h2>
-              Advisor
-            </h2>
-          </Grid>
-          <Grid item xs={8}>
-            <h2>
-              {advisor}
-            </h2>
-          </Grid>
+        <Grid item xs={2}>
+          <Chip label="Student" variant="outlined" />
         </Grid>
-        <h2>Members</h2>
-
-        {memberList.map(m => <div>{m}</div>)}
-
-      </Item>
-
-      <Item>
-        <Stack direction="row" spacing={1}>
-          <Chip label="6/6 Members"/>
-          <Button variant={"contained"} onClick={handleNavChange(MESSAGES_TAB)}>Request to Join</Button>
-          <Button variant={"contained"} onClick={handleNavChange(PROJECT_TAB)}>Edit</Button>
-        </Stack>
-      </Item>
-
-    </Item>
+        <Grid item xs={2}>
+          <Chip label={moment(date).format("MM/DD/YYYY")} variant="outlined" />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            {summary}
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6">Skills</Typography>
+          <List>
+            {skillsList.map((skill) => (
+              <ListItem divider>
+                <ListItemText>
+                  <Typography variant="body1">{skill}</Typography>
+                </ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6">Software</Typography>
+          <List>
+            {softwareList.map((software) => (
+              <ListItem divider>
+                <ListItemText>
+                  <Typography variant="body1">{software}</Typography>
+                </ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">Advisor: {advisor ? advisor : ""}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">Members</Typography>
+          <List>
+            {memberList.map((member) => (
+              <ListItem divider>
+                <ListItemText>
+                  <Typography variant="body1">{member.name} - {member.contact}</Typography>
+                </ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+      </Grid>
+    </Paper>
   </Grid>
 }
 
--- src/components/SeekingPost.js
+++ src/components/SeekingPost.js
@@ -1,57 +1,51 @@
 import React from 'react';
 import Grid from "@mui/material/Grid";
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, Paper, Typography, Chip, List, ListItem, ListItemText } from "@mui/material";
 import { ACTIVE_TAB, Item, MESSAGES_TAB, POST_TAB, SORT_BY, STATE } from "../util/Consts";
 import GlobalState from "../state/GlobalState";
+import moment from 'moment';
 
-const SeekingPost = ({post: {title, details, name, contact}}) => {
+const SeekingPost = ({ post: { title, date, summary, name, preferredContact, memberList } }) => {
 
   const {
-    [STATE]: {[ACTIVE_TAB]: activeTab, [SORT_BY]: sortBy}, hideDrawer, handleNavChange, hideDrawerAndSetTab,
+    [STATE]: { [ACTIVE_TAB]: activeTab, [SORT_BY]: sortBy }, hideDrawer, handleNavChange, hideDrawerAndSetTab,
   } = React.useContext(GlobalState);
 
   return <Grid item xs={6}>
-    <Item>
-      <Item>
-        <h1>
-          <Stack direction="row" spacing={1}>
-            {title}
-          </Stack>
-        </h1>
-        {details}
-        <Grid container item spacing={2}>
-          <Grid item xs={4}>
-            <h2>
-              Name
-            </h2>
-          </Grid>
-          <Grid item xs={8}>
-            <h2>
-              {name}
-            </h2>
-          </Grid>
+    <Paper elevation={3}>
+      <Grid container justifyContent="center">
+        <Grid item xs={10}>
+          <Typography variant="h4">{title}</Typography>
         </Grid>
-        <Grid container item spacing={2}>
-          <Grid item xs={4}>
-            <h2>
-              Contact
-            </h2>
-          </Grid>
-          <Grid item xs={8}>
-            <h2>
-              {contact}
-            </h2>
-          </Grid>
+        <Grid item xs={2}>
+          <Chip label={moment(date).format("MM/DD/YYYY")} variant="outlined" />
         </Grid>
-      </Item>
-      <Item>
-        <Stack direction="row" spacing={1}>
-          <Button variant={"contained"} onClick={handleNavChange(POST_TAB)}>Add to My Group</Button>
-          <Button variant={"contained"} onClick={handleNavChange(MESSAGES_TAB)}>Message</Button>
-        </Stack>
-      </Item>
-    </Item>
-  </Grid>;
+        <Grid item xs={6}>
+          <Typography variant="h6">{"Student Name"}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6">{preferredContact}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            {summary}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">Members</Typography>
+          <List>
+            {memberList.map((member) => (
+              <ListItem divider>
+                <ListItemText>
+                  <Typography variant="body1">{member.name} - {member.contact}</Typography>
+                </ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+      </Grid>
+    </Paper>
+  </Grid>
 }
 
 export default SeekingPost;
