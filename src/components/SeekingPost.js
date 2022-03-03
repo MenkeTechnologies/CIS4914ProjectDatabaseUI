51c9175edc2b89de12e512dcf62398785704d176
Carlson Joseph <carl2603@gmail.com>
2022-03-02 21:46:45 -0500

    Add views for offer and seeking posts.

--- src/components/SeekingPost.js
+++ src/components/SeekingPost.js
@@ -5,18 +5,13 @@ import { ACTIVE_TAB, Item, MESSAGES_TAB, POST_TAB, SORT_BY, STATE } from "../uti
 import GlobalState from "../state/GlobalState";
 import moment from 'moment';
 
-<<<<<<< HEAD
 const SeekingPost = ({ post: { title, date, summary, name, preferredContact, memberList } }) => {
-=======
-const SeekingPost = ({post: {title, summary, author: {name, type}, preferredContact}}) => {
->>>>>>> 6dd2aafc0cb534ddb4b27d002e314d93c3dbd576
 
   const {
     [STATE]: { [ACTIVE_TAB]: activeTab, [SORT_BY]: sortBy }, hideDrawer, handleNavChange, hideDrawerAndSetTab,
   } = React.useContext(GlobalState);
 
   return <Grid item xs={6}>
-<<<<<<< HEAD
     <Paper elevation={3}>
       <Grid container justifyContent="center">
         <Grid item xs={10}>
@@ -24,39 +19,6 @@ const SeekingPost = ({post: {title, summary, author: {name, type}, preferredCont
         </Grid>
         <Grid item xs={2}>
           <Chip label={moment(date).format("MM/DD/YYYY")} variant="outlined" />
-=======
-    <Item>
-      <Item>
-        <h1>
-          <Stack direction="row" spacing={1}>
-            {title}
-          </Stack>
-        </h1>
-        {summary}
-        <Grid container item spacing={2}>
-          <Grid item xs={4}>
-            <h2>
-              Name
-            </h2>
-          </Grid>
-          <Grid item xs={8}>
-            <h2>
-              {name}
-            </h2>
-          </Grid>
-        </Grid>
-        <Grid container item spacing={2}>
-          <Grid item xs={4}>
-            <h2>
-              Contact
-            </h2>
-          </Grid>
-          <Grid item xs={8}>
-            <h2>
-              {preferredContact}
-            </h2>
-          </Grid>
->>>>>>> 6dd2aafc0cb534ddb4b27d002e314d93c3dbd576
         </Grid>
         <Grid item xs={6}>
           <Typography variant="h6">{"Student Name"}</Typography>
